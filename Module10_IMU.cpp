922e457eadf0a12131dd9f987b1fc50f140ed145
Josh Lowe <lowe.joshua.41@gmail.com>
2019-11-12 19:57:58 -0500

    Add Euler functionality

--- Module10_IMU.cpp
+++ Module10_IMU.cpp
@@ -3,40 +3,56 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+//#define FEATURE_EVENT
+#define FEATURE_EULER
+
+
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 void setup(void)
 {
-  Serial.begin(115200);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
+    Serial.begin(115200);
+    Serial.println("Orientation Sensor Test"); Serial.println("");
 
-   //nitialise the sensor
-  if(!bno.begin())
-  {
-     //There was a problem detecting the BNO055 ... check your connections
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
+    //nitialise the sensor
+    if(!bno.begin())
+    {
+        //There was a problem detecting the BNO055 ... check your connections
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while(1);
+    }
 
-  delay(1000);
+    delay(1000);
 
-  bno.setExtCrystalUse(true);
+    bno.setExtCrystalUse(true);
 }
 
 void loop(void)
 {
-   //Get a new sensor event
-  sensors_event_t event;
-  bno.getEvent(&event);
+#ifdef FEATURE_EVENT
+    //Get a new sensor event
+    sensors_event_t event;
+    bno.getEvent(&event);
 
-   //Display the floating point data
-  Serial.print("X: ");
-  Serial.print(event.orientation.x, 4);
-  Serial.print("\tY: ");
-  Serial.print(event.orientation.y, 4);
-  Serial.print("\tZ: ");
-  Serial.print(event.orientation.z, 4);
-  Serial.println("");
+    //Display the floating point data
+    Serial.print("X: ");
+    Serial.print(event.orientation.x, 4);
+    Serial.print("\tY: ");
+    Serial.print(event.orientation.y, 4);
+    Serial.print("\tZ: ");
+    Serial.print(event.orientation.z, 4);
+    Serial.println("");
+#elif defined(FEATURE_EULER)
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("X: ");
+    Serial.print(euler.x());
+    Serial.print(" Y: ");
+    Serial.print(euler.y());
+    Serial.print(" Z: ");
+    Serial.print(euler.z());
+    Serial.println("");
 
-  delay(100);
+#endif
+
+    delay(100);
 }
